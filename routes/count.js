e52f60c280aabbf60699cd8d149b0de1f9cd771e
Sam Gussman <sgussman@stanford.edu>
2015-02-28 23:25:17 -0800

    Updating prototype to change the labels buttons, and add an additional info section.

--- routes/count.js
+++ routes/count.js
@@ -10,13 +10,15 @@ exports.view = function(req, res){
 exports.update = function(req, res){
 	var name = req.body.name;
 	var action = req.body.action;
+	var info = req.body.additionalInfo;
 	var date = new Date();
 	var time = date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
 	console.log(req.body);
 	console.log("Name: "+name);
 	console.log("Action: "+action);
 	console.log("Time: "+time);
-	var temp = {action: action, name: name, time: time};
+	console.log("Comment: "+info);
+	var temp = {action: action, name: name, time: time, comment: info};
 	console.log("Before clicks: "+clicks);
 	clicks.push(temp);
 	console.log("After clicks: "+clicks);
